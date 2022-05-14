e678627a611d64a4cebf563233caa08ee32e98fa
CetO-hub <skynet@SkyNet.localdomain>
2022-05-14 12:52:03 +0200

    Include display entered number and clear function

--- js/javascript.js
+++ js/javascript.js
@@ -25,3 +25,29 @@ function operate(operator, number1, number2) {
       return divide(number1, number2);
   }
 }
+
+function displayNumber(e) {
+  if (
+    isNumber1Array.find((element) => element === ".") !== undefined &&
+    e.target["innerText"] === "."
+  )
+    return;
+  isNumber1Array.push(e.target["innerText"]);
+  isDisplay.innerText = isNumber1Array.join("");
+}
+
+function clearNumber() {
+  isNumber1Array = [];
+  isNumber2Array = [];
+  isDisplay.innerText = 0;
+}
+
+let isNumber1Array = [];
+let isNumber2Array = [];
+let isDisplay = document.querySelector(".display");
+let isClear = document.querySelector(".clear");
+let isEnter = document.querySelector(".enter");
+let isButtons = document.querySelectorAll(".number");
+
+isButtons.forEach((button) => button.addEventListener("click", displayNumber));
+isClear.addEventListener("click", clearNumber);
