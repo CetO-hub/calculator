5e83b42134045923f5c0753828d0fac61e4dcfeb
CetO-hub <skynet@SkyNet.localdomain>
2022-05-14 14:14:01 +0200

    Introduce save user entries and clear
    Generate an array of the users typed numbers and after pressing
    an operator symbol by the user the function saves the
    second number in an array.

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
-function add(number1, number2) {
-  return number1 + number2;
+function add(isNumber1Array, isNumber2Array) {
+  return console.log(isNumber1Array, isNumber2Array);
 }
 
 function subtract(number1, number2) {
@@ -13,41 +13,72 @@ function multiply(number1, number2) {
 function divide(number1, number2) {
   return number1 / number2;
 }
-function operate(operator, number1, number2) {
-  switch (operator) {
-    case "+":
-      return add(number1, number2);
+function operate(isOperator) {
+  switch (1) {
+    case 1:
+      add(isNumber1Array, isNumber2Array);
+      console.log("Hey");
+      break;
     case "-":
-      return subtract(number1, number2);
+      subtract(isNumber1Array, isNumber2Array);
+      break;
     case "*":
-      return multiply(number1, number2);
-    case "*":
-      return divide(number1, number2);
+      multiply(isNumber1Array, isNumber2Array);
+      break;
+    case "/":
+      divide(isNumber1Array, isNumber2Array);
+      break;
   }
 }
 
 function displayNumber(e) {
   if (
-    isNumber1Array.find((element) => element === ".") !== undefined &&
-    e.target["innerText"] === "."
-  )
+    (isNumber1Array.find((element) => element === ".") !== undefined &&
+      e.target["innerText"] === ".") ||
+    (isNumber2Array.find((element) => element === ".") !== undefined &&
+      e.target["innerText"] === ".")
+  ) {
     return;
-  isNumber1Array.push(e.target["innerText"]);
-  isDisplay.innerText = isNumber1Array.join("");
+  }
+  if (e.target["className"] === "operator") {
+    isOperator = e.target["innerText"];
+    isDisplay.innerText = isOperator;
+    return;
+  }
+  if (!isOperator) {
+    isNumber1Array.push(e.target["innerText"]);
+    isDisplay.innerText = isNumber1Array.join("");
+    console.log(isNumber1Array);
+    return;
+  }
+  if (isOperator) {
+    isNumber2Array.push(e.target["innerText"]);
+    isDisplay.innerText = isNumber2Array.join("");
+    console.log(isNumber2Array);
+    return;
+  }
 }
 
 function clearNumber() {
   isNumber1Array = [];
   isNumber2Array = [];
+  isOperator = undefined;
   isDisplay.innerText = 0;
 }
 
 let isNumber1Array = [];
 let isNumber2Array = [];
+let isOperator;
+
 let isDisplay = document.querySelector(".display");
 let isClear = document.querySelector(".clear");
 let isEnter = document.querySelector(".enter");
 let isButtons = document.querySelectorAll(".number");
+let isOperators = document.querySelectorAll(".operator");
 
 isButtons.forEach((button) => button.addEventListener("click", displayNumber));
+isOperators.forEach((operator) =>
+  operator.addEventListener("click", displayNumber)
+);
 isClear.addEventListener("click", clearNumber);
+isEnter.addEventListener("click", operate);
